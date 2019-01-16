c09b66854d3dc8cb0d179afd264f832c91ea6684
MozyOk <mozy.okubo@gmail.com>
2019-01-17 00:20:12 +0900

    add bluma fontawesome song list json

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'bulma/css/bulma.css'
+
+import FontAwesome from '@fortawesome/fontawesome'
+import VueFontAwesome from '@fortawesome/vue-fontawesome'
+import FontAwesomeSolid from '@fortawesome/fontawesome-free-solid'
+
+FontAwesome.library.add(FontAwesomeSolid)
+
+Vue.component('fa-icon', VueFontAwesome)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
